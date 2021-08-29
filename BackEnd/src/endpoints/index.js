9943681bd0868478169696b25517316af623f0d5
LeonardBol <leonardo.bolanos@correounivalle.edu.co>
2021-08-29 10:37:42 -0500

    Controller For Clients - Ready

--- BackEnd/src/endpoints/index.js
+++ BackEnd/src/endpoints/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { getEmpleados, createEmpleado,
     getEmpleadoById, deleteEmpleado,
     updateEmpleado } = require('../controllers/empleadoController');
-
+const { getClients, createClient, deleteClient } = require('../controllers/clientsController');
 const router = Router();
 
 //Empleados:
@@ -11,5 +11,9 @@ router.get('/empleado/:id', getEmpleadoById)
 router.post('/empleado', createEmpleado);
 router.delete('/empleado/:id', deleteEmpleado);
 router.put('/empleado/:id', updateEmpleado);
+
+router.get('/client', getClients);
+router.post('/client', createClient);
+router.delete('/client/:id', deleteClient);
 
 module.exports = router
